8c9ce8453241086f84b55bcab80f105102a227af
GaGa <o.kimhong168@gmail.com>
2025-05-15 20:37:46 +0700

    navbar

--- Client/src/components/Navbar.jsx
+++ Client/src/components/Navbar.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
-// import {useAuthStore} from './store/authStore';
+import { useAuthStore } from '../store/useAuthStore';
 
 const Navbar = () => {
-  // const {authUser} = useAuthStore();
+  const {logout,authUser} = useAuthStore();
   return (
-    <div>Navbar</div>
+    <header
+    className='bg-base-100 border-b border-base-300 fixed w-full top-0 z-40 backdrop-blur-lg bg-base-100/80'>
+      <div className="container mx-auto px-4 h-16">
+    <div className="flex items-center justify-between h-full">
+    <div className='flex items-center gap-8'>
+    <Link to="/" className="flex items-center gap-2.5 hover:opacity-80 transition-all">
+      <div className='w-9 h-9 rounded-lg bg-primary/10 flex items-center justify-center'>
+      <MessageSqaure className="w-5 h-5 text-primary" />
+        
+      </div>
+      <h1 className='text-lg font-bold'>Chatty</h1>
+    </Link>
+    </div>
+    </div>
+      </div>
+    </header>
   )
 }
 
